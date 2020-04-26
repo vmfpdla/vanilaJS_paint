0d2b322ae1cdfc181746fe31f725ea6957ab9c20
vmfpdla <vmfpdla0@naver.com>
2020-04-26 12:30:47 +0900

    complete

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const canvas = document.getElementById("jsCanvas");
-
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 
-canvas.width = 700;
-canvas.height = 700;
-ctx.strokeStyle = "#2c2c2c";
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
+
 let painting = false; // 기본적으로 false .그러나 내가 캔버스에 그림을 그리기 시작하면 true
+let filling = false;
 
 function startPainting() {
     painting = true;
@@ -38,6 +50,41 @@ function onMouseUp(event) {
     stopPainting();
 }
 
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+    const range = event.target.value;
+    ctx.lineWidth = range;
+}
+
+function handleModeClick(event) {
+    if (filling === true) {
+        filling = false;
+        mode.innerText = "FILL";
+    } else {
+        filling = true;
+        mode.innerText = "PAINT";
+    }
+}
+
+function handleCanvasClick(event) {
+    if (filling === true) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+}
+
+function handleSaveClick(event) {
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJs";
+    link.click();
+
+}
 
 
 if (canvas) { // canvas 안에서만 실행되도록 !
@@ -66,4 +113,23 @@ if (canvas) { // canvas 안에서만 실행되도록 !
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+}
+
+
+if (colors) {
+    Array.from(colors).forEach(color =>
+        color.addEventListener("click", handleColorClick));
+}
+
+if (range) {
+    range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+    mode.addEventListener("click", handleModeClick);
+}
+
+if (save) {
+    save.addEventListener("click", handleSaveClick);
 }
